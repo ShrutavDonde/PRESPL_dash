8660ae84b22ea21cb64685f078eab9d6960ed5ba
ShrutavDonde <shrutavdonde@gmail.com>
2025-07-18 11:48:55 +0530

    Added more pie charts

--- pages/Plots.py
+++ pages/Plots.py
@@ -13,12 +13,18 @@ if uploaded_file:
     
     zones = st.sidebar.multiselect("Select Zone", df["Zone/Intercompany"].unique())
     verticals = st.sidebar.multiselect("Select Business Vertical", df["Business Vertical"].unique())
+    locations = st.sidebar.multiselect("Select Location", df["Location"].unique())
+    ratings = st.sidebar.multiselect("Select Rating", df["Rating"].unique())
     
     filtered_df = df.copy()
     if zones:
         filtered_df = filtered_df[filtered_df["Zone/Segment"].isin(zones)]
     if verticals:
         filtered_df = filtered_df[filtered_df["Business Vertical"].isin(verticals)]
+    if locations:
+        filtered_df = filtered_df[filtered_df['Location'].isin(locations)]
+    if ratings:
+        filtered_df = filtered_df[filtered_df['Rating'].isin(ratings)]
 
     col1, col2 = st.columns(2)
 
@@ -28,11 +34,21 @@ if uploaded_file:
         fig1 = px.pie(pie1_data, values='DrTotal', names='Business Vertical', title="By Business Vertical")
         st.plotly_chart(fig1, use_container_width=True)
 
+        st.subheader("📍 DrTotal by Location")
+        pie3_data = filtered_df.groupby('Location')['DrTotal'].sum().reset_index()
+        fig3 = px.pie(pie3_data, values='DrTotal', names='Location', title="By Location")
+        st.plotly_chart(fig3, use_container_width=True)
+
     with col2:
         st.subheader("🌍 DrTotal by Zone/Intercompany")
         pie2_data = filtered_df.groupby('Zone/Intercompany')['DrTotal'].sum().reset_index()
         fig2 = px.pie(pie2_data, values='DrTotal', names='Zone/Intercompany', title="By Zone/Intercompany")
         st.plotly_chart(fig2, use_container_width=True)
+
+        st.subheader("⭐ DrTotal by Rating")
+        pie4_data = filtered_df.groupby('Rating')['DrTotal'].sum().reset_index()
+        fig4 = px.pie(pie4_data, values='DrTotal', names='Rating', title="By Rating")
+        st.plotly_chart(fig4, use_container_width=True)
 
     # Horizontal divider
     st.markdown("---")
